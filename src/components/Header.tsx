934e1a0020e1af0524ca31929e3a7c700397f7ba
unknown <ricopozzi@hotmail.com>
2022-07-01 14:12:30 -0300

    fixed ONE LETTER

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ export function Header({ ...rest }) {
   return (
     <div {...rest}>
       <Link href='/home'>
-        <Image src='/rhemaPreto.png' width={220} height={50} />
+        <Image src='/rhemapreto.png' width={220} height={50} />
       </Link>
     </div>
   );
