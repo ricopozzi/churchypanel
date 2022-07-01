68aba4c5eae6283df4dda2f99bf5f56d6c7b8e3a
unknown <ricopozzi@hotmail.com>
2022-07-01 14:09:38 -0300

    auth fixed

--- src/pages/api/notify.ts
+++ src/pages/api/notify.ts
@@ -1,18 +1,10 @@
 import Cors from 'cors'
-import initMiddleware from '../../lib/init-middleware'
 import axios from 'axios'
 import { NextApiRequest, NextApiResponse } from 'next'
 
-const cors = initMiddleware(
-  Cors({
-    methods: ['GET', 'POST', 'OPTIONS'],
-  })
-)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await cors(req, res)
   const {to, title, body} = req.body.data
-  console.log(to,title,body)
 
         const { data, status } = await axios.post(
             "https://exp.host/--/api/v2/push/send",
@@ -31,7 +23,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             }
           );
         
-        console.log(status)
         
         return res.json({data})
    
